098de235543d00ff887ecb630a8a82dc74dd7dfc
crystal-twinkle <autoro1@mail.ru>
2024-06-14 17:33:42 +0300

    change function event countdown

--- static/js/main/event-countdown.js
+++ static/js/main/event-countdown.js
@@ -4,11 +4,11 @@ console.log(next_event_time);
 let parts = next_event_time.split(/[- :]/);
 let eventDate = new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
 
-function  eventCountdown() {
+function eventCountdown() {
   let now = new Date();
   let difference = eventDate - now;
 
-
+  if (difference > 0) {
     let days = Math.floor(difference / (1000 * 60 * 60 * 24));
     let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
@@ -18,7 +18,8 @@ function  eventCountdown() {
     document.getElementById("countdown__hours").innerText = `${hours}`;
     document.getElementById("countdown__mins").innerText = `${minutes}`;
     document.getElementById("countdown__secs").innerText = `${seconds}`;
+  }
 }
 
-// Обновляем счетчик каждую секунду
+// Update the counter every second
 let timer = setInterval(eventCountdown, 1000);
