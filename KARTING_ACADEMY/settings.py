cfb1e348899d5ddfa21c698cbbf25c683b0ceeed
Maksim Volosh <maksgoza9@gmail.com>
2024-10-05 22:59:07 +0300

    Add conf

--- KARTING_ACADEMY/settings.py
+++ KARTING_ACADEMY/settings.py
@@ -2,16 +2,9 @@ import os
 from pathlib import Path
 
 from django.utils.translation import gettext_lazy as _
-
-from config import PASS, SECRET_KEY
+from config import PASS
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = SECRET_KEY
-
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = [
     # ...
@@ -133,10 +126,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    from .prod_settings import *
